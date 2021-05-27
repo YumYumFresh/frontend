8d85260905f54421c2f6108fea55e9eea383e59f
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-05-27 15:41:46 -0400

    took away <br> from farmers market js

--- src/components/MarketCard.js
+++ src/components/MarketCard.js
@@ -3,38 +3,39 @@ import Card from "react-bootstrap/Card";
 import axios from "axios";
 import "../css/MarketCard.css";
 
-const MarketCard = ({ id, marketName}) => {
+const MarketCard = ({ id, marketName }) => {
   const [marketAddress, setMarketAddress] = useState("");
   const [marketLink, setMarketLink] = useState("");
   const [marketProducts, setMarketProducts] = useState("");
   const [marketSchedule, setMarketSchedule] = useState("");
-  const link =  "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id
+  const link =
+    "https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id;
 
   useEffect(() => {
-    axios
-      .get(
-        link
-      )
-      .then((response) => {
-       //console.log(response.data.marketdetails.Address);
-        setMarketAddress(response.data.marketdetails.Address);
-        setMarketLink(response.data.marketdetails.GoogleLink);
-        setMarketProducts(response.data.marketdetails.Products);
-        setMarketSchedule(response.data.marketdetails.Schedule);
-        console.log(response.data.marketdetails.Schedule);
-      });
+    axios.get(link).then((response) => {
+      //console.log(response.data.marketdetails.Address);
+      setMarketAddress(response.data.marketdetails.Address);
+      setMarketLink(response.data.marketdetails.GoogleLink);
+      setMarketProducts(response.data.marketdetails.Products);
+      setMarketSchedule(response.data.marketdetails.Schedule.replace(/<br\s*\/?>/gi,' '));
+    });
   }, [link]);
-
+  
+  console.log(" market schedule ", marketSchedule);
   return (
-    <Card style={{margin:"3%"}} className="marketCard">
+    <Card style={{ margin: "3%" }} className="marketCard">
       <Card.Body>
         <div className="marketCard__a">
-            <a href={marketLink} target="_blank" rel="noreferrer" className="marketCard__link">
-              Map It!
-            </a>
+          <a
+            href={marketLink}
+            target="_blank"
+            rel="noreferrer"
+            className="marketCard__link"
+          >
+            Map It!
+          </a>
         </div>
-        <Card.Title>{marketName}
-        </Card.Title>
+        <Card.Title>{marketName}</Card.Title>
         <Card.Text>
           <div>{marketAddress}</div>
           <div>{marketProducts}</div>
